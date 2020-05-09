20657e3ee375b0cccbba9d7a5e599371b59646ce
Ricky Jiang <ricky.j@anvo.com>
2020-05-09 17:35:47 +0800

    auto central earth's position

--- frontend/src/Map.ts
+++ frontend/src/Map.ts
@@ -9,12 +9,14 @@ import {FeatureCollection} from "@amcharts/amcharts4-geodata/.internal/Geodata";
 import {Animation} from "@amcharts/amcharts4/.internal/core/utils/Animation";
 import {MapManager} from "./MapManager";
 import {CountriesData} from "./models/MapData";
+import {AppManager} from "./AppManager";
 
 am4core.useTheme(am4themes_animated);
 
 export class Map {
     chart:any;
     animatable = false;
+    scale:number = 0.6;
     constructor(data: CountriesData, dataField: string) {
         this.initial();
         this.limitVerticalRotate();
@@ -25,6 +27,7 @@ export class Map {
                 new Legend(dataField);
             }
         }
+        this.scaleMap();
     }
     initial() {
         this.chart = MapManager.createChart("chartdiv");
@@ -35,12 +38,6 @@ export class Map {
         this.setChartPolygonColor();
         this.chart.deltaLongitude = 0;
         this.chart.deltaLatitude = 0;
-        this.chart.seriesContainer.element.scale = 0.8;
-        this.chart.seriesContainer.events.on("transformed", () => {
-            this.chart.seriesContainer.element.x = 100;
-            this.chart.seriesContainer.element.y = 65;
-        });
-
         // 移动方式
         this.chart.panBehavior = "rotateLongLat";
     }
@@ -84,5 +81,30 @@ export class Map {
         this.chart.seriesContainer.events.on("down", function() {
             earthAnimateObj && earthAnimateObj.kill();
         });
+    }
+    scaleMap() {
+        this.chart.seriesContainer.events.on('ready', (ev:any) => {
+            this.chart.events.disable();
+            this.chart.seriesContainer.events.disable();
+            const seriesElement = this.chart.seriesContainer.element;
+            seriesElement.scale = this.scale;
+            this.centralContainer();
+            seriesElement.transform = this.centralContainer.bind(this, true);
+            AppManager.events.triggerEvent('MapReady');
+        });
+    }
+    centralContainer(setAttribute?:boolean) {
+        const fullWidth = window.innerWidth;
+        const fullHeight = window.innerHeight;
+        const polygonBox = MapManager.polygonSeries.dom.getBBox();
+        const seriesElement = this.chart.seriesContainer.element;
+        const x = Math.ceil((fullWidth / 2) - (polygonBox.x * this.scale) - (polygonBox.width * this.scale / 2));
+        const y = Math.ceil((fullHeight / 2) - (polygonBox.height * this.scale / 2));
+        seriesElement.x = x;
+        seriesElement.y = y;
+
+        if (setAttribute) {
+            seriesElement.node.setAttribute('transform', `translate(${x}, ${y}) scale(${this.scale})`);
+        }
     }
 }
