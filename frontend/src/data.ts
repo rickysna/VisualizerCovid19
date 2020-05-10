91bdf557f76ea83dc2c52661555b84bb22259e1d
Ricky Jiang <ricky.j@anvo.com>
2020-05-10 18:09:32 +0800

    save

--- frontend/src/data.ts
+++ frontend/src/data.ts
@@ -1,7 +1,7 @@
-import {API, CountriesData} from "./models/MapData";
+import {API} from "./models/MapData";
 
 export async function getCountriesData():Promise<API> {
-    // return await fetch('https://vq0rwefgvd.execute-api.us-east-1.amazonaws.com/dev/api')
-    return await fetch('http://localhost:3000/dev/api')
+    // return await fetch('http://localhost:3000/dev/api')
+    return await fetch('https://vq0rwefgvd.execute-api.us-east-1.amazonaws.com/dev/api')
         .then(result => result.json());
 }
