b2ae118884b9bd4f12111f0ecdc4756a53e17b28
ricky_sna <mr.jiangxue@hotmail.com>
2020-07-16 06:20:56 +0800

    add events

--- frontend/src/events.ts
+++ frontend/src/events.ts
@@ -1,10 +1,14 @@
+export const ModelReady = "model_ready";
+
 export const MapModelDispatchMapData = "map_model_dispatch_map_data";
 export const MapModelGetMapData = "map_model_get_map_data";
 export const MapModelDispatchCountriesData = "map_model_dispatch_countries_data";
 export const MapModelGetCountriesData = "map_model_get_countries_data";
 export const MapModelDispatchCountriesDataByCases = "map_model_dispatch_countries_data_by_cases";
 export const MapModelGetCountriesDataByCases = "map_model_get_countries_data_by_cases";
-export const ModelReady = "model_ready";
 export const MapModelUpdateSelectedCountryID = "map_model_update_selected_country_id";
+export const MapModelGetSelectedCountryID = "map_model_get_selected_country_id";
 
 export const RankingViewUpdateCountryID = "ranking_view_update_country_id";
+
+export const ChartComponentMoveAnimation = "chart_component_move_animation";
