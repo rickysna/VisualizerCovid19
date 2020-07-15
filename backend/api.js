2146ec7f699b7986f2a51d7a6b0d90c5923faa5a
ricky_sna <mr.jiangxue@hotmail.com>
2020-07-16 06:24:59 +0800

    save

--- backend/api.js
+++ backend/api.js
@@ -12,7 +12,4 @@ module.exports.handler = async () => {
     },
     body: JSON.stringify(data),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
